17173abb7c14a7bffd7c4aa0efa8edba7f61ceae
patrickvonk@hotmail.com <patrickvonk@hotmail.com>
2016-02-15 09:30:15 +0100

    Sebas is weer gay net als zijn moeder

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 var mark = 626564324234;
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Sebas is gay net als zijn moeder');
+});
+
+app.listen(4200, function() {
+    console.log('hot and ready to go');
+});
